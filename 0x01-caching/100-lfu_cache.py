51faff65192a785ed07b090b43c6860d22ad5037
elsaaeid <92111076+elsaaeid@users.noreply.github.com>
2024-07-24 11:51:00 +0300

    Update 100-lfu_cache.py

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """LFUCache Module"""
-from collections import defaultdict, OrderedDict
+from collections import defaultdict, deque
 BaseCaching = __import__('base_caching').BaseCaching
 
 
@@ -12,9 +12,18 @@ class LFUCache(BaseCaching):
     def __init__(self):
         """Initialize"""
         super().__init__()
-        self.frequency_list = defaultdict(OrderedDict)
-        self.key_node = {
-        self.frequency = {}
+        '''Dictionary to store
+        the frequency of each key
+        '''
+        self.frequency = defaultdict(int)
+        '''Dictionary to store
+        keys based on frequency
+        '''
+        self.frequency_queue = defaultdict(deque)
+        '''
+        Minimum frequency in the cache
+        '''
+        self.min_frequency = 0
 
     def put(self, key, item):
         """
@@ -25,14 +34,15 @@ class LFUCache(BaseCaching):
 
         if key in self.cache_data:
             self.cache_data[key] = item
-            self.update_frequency_of_key(key)
+            self.frequency[key] += 1
         else:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
                 self.discard_least_frequently_used_item()
 
             self.cache_data[key] = item
-            self.frequency[key] = [1]
-            self.add_key_to_frequency_list(key)
+            self.frequency[key] = 1
+            self.frequency_queue[1].append(key)
+            self.min_frequency = 1
 
     def get(self, key):
         """
@@ -41,34 +51,28 @@ class LFUCache(BaseCaching):
         if key is None or key not in self.cache_data:
             return None
 
-        self.update_frequency_of_key(key)
+        self.frequency[key] += 1
+        self.update_frequency_queue(key)
         return self.cache_data[key]
-
-    def update_frequency_of_key(self, key):
-        """
-        Update frequency key
-        """
-        frequency = self.frequency[key][0]
-        del self.frequency_list[frequency][key]
-        self.frequency[key][0] += 1
-        self.add_key_to_frequency_list(key)
-
-    def add_key_to_frequency_list(self, key):
-        """
-        Add key to frequency list
-        """
-        frequency = self.frequency[key][0]
-        self.frequency_list[frequency][key] = None
-        self.key_node[key] = next(iter(self.frequency_list[frequency]))
 
     def discard_least_frequently_used_item(self):
         """
         Delete min key which used frequently
         """
-        min_frequency = min(self.frequency_list.keys())
-        key_to_discard = self.key_node[min_frequency]
+        while not self.frequency_queue[self.min_frequency]:
+            self.min_frequency += 1
+
+        key_to_discard = self.frequency_queue[self.min_frequency].popleft()
         del self.cache_data[key_to_discard]
         del self.frequency[key_to_discard]
-        del self.key_node[key_to_discard]
-        del self.frequency_list[min_frequency][key_to_discard]
         print("DISCARD:", key_to_discard)
+
+    def update_frequency_queue(self, key):
+        """
+        Update frequency key
+        """
+        frequency = self.frequency[key]
+        self.frequency_queue[frequency-1].remove(key)
+        self.frequency_queue[frequency].append(key)
+        if not self.frequency_queue[self.min_frequency]:
+            self.min_frequency += 1
